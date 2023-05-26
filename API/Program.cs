2178ab65afdf51c94cc7ef0888606086725792a6
Tanuja Sharma <84020250+Tanujasharma@users.noreply.github.com>
2023-05-26 20:19:46 +0530

    added Client(UI) walking skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,9 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
@@ -18,6 +21,9 @@ builder.Services.AddDbContext<DataContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+//app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 // if (app.Environment.IsDevelopment())
 // {
 //     app.UseSwagger();
