f96cb55a8f58d9358e740c22a8ed6f76d470cfe0
Tanuja Sharma <84020250+Tanujasharma@users.noreply.github.com>
2023-05-26 14:02:55 +0530

    completed the api project skeleton

---
+++ API/Data/Migrations/20230526072426_InitialCreateMinorChange.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateMinorChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "userName",
+                table: "Users",
+                newName: "UserName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "userName");
+        }
+    }
+}
