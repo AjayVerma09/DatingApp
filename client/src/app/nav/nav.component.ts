3c2895fe3e7e3cfad9f3273f63a782da37ca8c10
Tanuja Sharma <84020250+Tanujasharma@users.noreply.github.com>
2023-05-31 23:46:08 +0530

    added home component and register comp

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -8,19 +10,10 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
 model: any = {};
-loggedIn = false;
 
-  constructor(private accountService: AccountService){}
+  constructor(public accountService: AccountService){}
 
   ngOnInit(): void {
-    this.getCurrentUSer();
-  }
-
-  getCurrentUSer() {
-    this.accountService.currentUser$.subscribe({
-      next: user => this.loggedIn = !!user,
-      error: error => console.log(error)
-    })
   }
 
   login() {
@@ -28,7 +21,6 @@ loggedIn = false;
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
       
@@ -37,7 +29,6 @@ loggedIn = false;
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
   }
 
 }
