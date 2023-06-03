f3f4b29eec2c92b2d6773cb2ffd84c768301e3c9
Tanuja Sharma <84020250+Tanujasharma@users.noreply.github.com>
2023-06-03 14:25:07 +0530

    added theme and shared module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,15 +8,13 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -34,12 +32,8 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    SharedModule,
     BrowserAnimationsModule,
-    TooltipModule.forRoot(),
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass:'toast-bottom-right'
-    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
